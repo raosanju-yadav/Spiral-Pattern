2aea0b0377bafa2ee73dcdf06e092a53713b5217
SanjuRao <raosanjurao366@gmail.com>
2024-11-04 20:16:21 +0530

    Add files via upload

---
+++ turtl.py
@@ -0,0 +1,10 @@
+import turtle
+sanju=turtle.Turtle()
+# sanju.speed(20)
+sanju.color("green","cyan")
+sanju.begin_fill()
+for i in range(100):
+    sanju.forward(200)
+    sanju.left(115)
+sanju.end_fill()
+turtle.done()
